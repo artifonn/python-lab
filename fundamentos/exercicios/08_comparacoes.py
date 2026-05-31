76baeb65d336296e978f077e1cc23acf9531c1ed
Sérgio Artifon <artifonn@gmail.com>
2026-05-30 23:00:32 -0300

    feat(fundamentos): adiciona exercício de operadores relacionais

---
+++ fundamentos/exercicios/08_comparacoes.py
@@ -0,0 +1,12 @@
+# Crie duas variáveis:
+# Mostre o resultado de todas as comparações possíveis.
+
+a = 10
+b = 20
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a <= b)
+print(a >= b)
