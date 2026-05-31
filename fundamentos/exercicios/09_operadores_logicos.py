c357dd9d126e7ec4c11553407740a8db382e21b4
Sérgio Artifon <artifonn@gmail.com>
2026-05-30 23:23:41 -0300

    feat(fundamentos): adiciona exercício de operadores lógicos

---
+++ fundamentos/exercicios/09_operadores_logicos.py
@@ -0,0 +1,13 @@
+# Faça comparações usando:
+  # and
+  # or
+  # not
+
+idade = 35
+possui_carteira = False
+
+print(idade >= 18 and possui_carteira)
+
+print(idade >= 18 or possui_carteira)
+
+print(not idade >= 18 or possui_carteira)
