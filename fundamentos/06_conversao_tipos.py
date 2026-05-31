8c92888781649e1a818fa3e0f95ad3065146ca6c
Sérgio Artifon <artifonn@gmail.com>
2026-05-30 21:48:02 -0300

    feat(fundamentos): adiciona exercício de conversão de tipos

---
+++ fundamentos/06_conversao_tipos.py
@@ -0,0 +1,18 @@
+# Enunciado
+
+# Solicite:
+
+# Idade
+# Altura
+
+# Converta os valores para seus respectivos tipos e exiba: 
+
+# Valor informado
+# Tipo do valor
+
+idade = int(input('Informe a sua idade: '))
+
+altura = float(input('Informe a sua altura: '))
+
+print('Você tem ' + str(idade) + ' e o tipo da variável é ' + str(type(idade)))
+print('A sua altura é ' + str(altura) + ' e o tipo da variável é ' + str(type(altura)))
