77e7e77257961af64cb87a5fba3d48bbef7c02c8
Sérgio Artifon <artifonn@gmail.com>
2026-05-31 14:17:47 -0300

    feat(estruturas-dados): adiciona exercício de listas com frutas favoritas

---
+++ estrutura_de_dados/exercicios/01_cadastro_de_frutas/01_frutas_favoritas.py
@@ -0,0 +1,23 @@
+
+# Crie uma lista contendo 5 frutas.
+
+# Exiba:
+
+# - A lista completa
+# - A primeira fruta da lista
+# - A última fruta da lista
+
+
+lista = ['Manga', 'Abacaxi', 'Morango', 'Mirtilo', 'Ameixa']
+
+print('****** Essa é a minha lista de frutas favoritas *******')
+for fruta in lista:
+  print(fruta)
+
+print('\n****** Essa é a primeira fruta da minha lista ******')
+print(lista[0])
+
+print('\n****** Essa é a ultima fruta da minha lista ******')
+print(lista[-1])
+
+
---
+++ estrutura_de_dados/exercicios/01_cadastro_de_frutas/README.md
@@ -0,0 +1,13 @@
+## Objetivo
+
+Aprender a criar listas e acessar seus elementos.
+
+## Enunciado
+
+Crie uma lista contendo 5 frutas.
+
+Exiba:
+
+- A lista completa
+- A primeira fruta da lista
+- A última fruta da lista
