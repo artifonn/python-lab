f35886d6c7dbb8ca5e707eaafc942eb7931889cb
Sérgio Artifon <artifonn@gmail.com>
2026-05-30 22:47:00 -0300

    feat(fundamentos): adiciona calculadora simples

---
+++ fundamentos/exercicios/07_calculadora_simples.py
@@ -0,0 +1,20 @@
+  # Crie uma calculadora simples.
+
+  # Peça dois números.
+
+# Mostre:
+
+  # Soma
+  # Subtração
+  # Multiplicação
+  # Divisão
+
+print('----------- Calculadora -----------')
+
+numero1 = int(input('Informe o primeiro número: '))
+numero2 = int(input('Informe o segundo número: '))
+
+print(f'Soma: {numero1 + numero2}')
+print(f'Subtração: {numero1 - numero2}')
+print(f'Multiplicação: {numero1 * numero2}')
+print(f'Divisão: {numero1 / numero2}')
