7e836cab2647dba6a11fcfa858206e40a72870ff
Sérgio Artifon <artifonn@gmail.com>
2026-05-31 21:41:37 -0300

    feat(estruturas-dados): adiciona exercício introdutório sobre dicionários

---
+++ estrutura_de_dados/exercicios/08_cadastro_cliente/08_cadastro__cliente.py
@@ -0,0 +1,9 @@
+usuario = {
+  'nome' : 'João',
+  'idade' : 35,
+  'cidade' : 'Brasília',
+  'email' : 'joao@email.com'
+}
+
+for chave, valor in usuario.items():
+  print(f'{chave} : {valor}')
---
+++ estrutura_de_dados/exercicios/08_cadastro_cliente/README.md
@@ -0,0 +1,14 @@
+## Objetivo
+
+Aprender a utilizar dicionários.
+
+## Enunciado
+
+Crie um dicionário contendo:
+
+- Nome
+- Idade
+- Cidade
+- E-mail
+
+Exiba todas as informações cadastradas.
