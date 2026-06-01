0d14f3fb81c0b8ff3431fcd76d9bc232d3408957
Sérgio Artifon <artifonn@gmail.com>
2026-05-31 22:04:32 -0300

    feat(estruturas-dados): adiciona exercício de acesso a dicionários

---
+++ estrutura_de_dados/exercicios/09_perfil_colaborador/09_perfil_colaborador.py
@@ -0,0 +1,9 @@
+funcionario = {
+    "nome": "Maria",
+    "cargo": "Engenheira de Software",
+    "departamento": "Tecnologia da Informação",
+    "tempo_empresa": "18 meses"
+}
+
+for chave, valor in funcionario.items():
+  print(f'{chave} : {valor}')
---
+++ estrutura_de_dados/exercicios/09_perfil_colaborador/README.md
@@ -0,0 +1,14 @@
+## Objetivo
+
+Praticar o acesso aos valores armazenados em dicionários.
+
+## Enunciado
+
+Crie um dicionário contendo:
+
+- Nome
+- Cargo
+- Departamento
+- Tempo de empresa
+
+Exiba cada informação individualmente.
