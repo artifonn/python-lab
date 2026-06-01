f44dbb151c24473fe54eaa04a1d81b23951c0041
Sérgio Artifon <artifonn@gmail.com>
2026-05-31 21:21:51 -0300

    feat(estruturas-dados): adiciona exercício introdutório sobre tuplas

---
+++ estrutura_de_dados/exercicios/06_registro_de_produto/06_registro_produto.py
@@ -0,0 +1,7 @@
+produto = ('Notebook', 3500, 'Eletrônicos')
+
+print('******** Produto ********\n')
+
+print(f'Nome: {produto[0]}')
+print(f'Preço: {produto[1]}')
+print(f'Categoria: {produto[2]}')
---
+++ estrutura_de_dados/exercicios/06_registro_de_produto/README.md
@@ -0,0 +1,13 @@
+## Objetivo
+
+Aprender o conceito de tuplas.
+
+## Enunciado
+
+Crie uma tupla contendo:
+
+- Nome do produto
+- Preço
+- Categoria
+
+Exiba cada informação individualmente.
