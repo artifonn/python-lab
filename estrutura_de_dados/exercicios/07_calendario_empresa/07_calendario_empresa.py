2fb1ebeb5307b80f6662035aa827514b98b83cd2
Sérgio Artifon <artifonn@gmail.com>
2026-05-31 21:31:16 -0300

    feat(estruturas-dados): adiciona exercício com tuplas com dias da semana

---
+++ estrutura_de_dados/exercicios/07_calendario_empresa/07_calendario_empresa.py
@@ -0,0 +1,10 @@
+dias_da_semana = ('Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-feira', 'Sexta-feira', 'Sabado')
+
+print('----------Primeiro dia da Semana ----------')
+print(dias_da_semana[0])
+
+print('\n-------- Ultimo Dia da Semana --------')
+print(dias_da_semana[-1])
+
+print('-------- Quantidade de dias Cadastrados --------')
+print(len(dias_da_semana))
---
+++ estrutura_de_dados/exercicios/07_calendario_empresa/README.md
@@ -0,0 +1,13 @@
+## Objetivo
+
+Praticar o uso de tuplas para armazenar dados fixos.
+
+## Enunciado
+
+Crie uma tupla contendo os sete dias da semana.
+
+Exiba:
+
+- Primeiro dia
+- Último dia
+- Quantidade de dias cadastrados
