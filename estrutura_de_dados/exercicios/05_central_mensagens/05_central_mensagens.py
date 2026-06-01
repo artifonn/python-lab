6e6ae137a725c812494ba5822ac813ce2661bb7c
Sérgio Artifon <artifonn@gmail.com>
2026-05-31 21:09:46 -0300

    feat(estruturas-dados): adiciona exercício de métodos de strings

---
+++ estrutura_de_dados/exercicios/05_central_mensagens/05_central_mensagens.py
@@ -0,0 +1,13 @@
+frase_motivacional = 'O sucesso não é construído em um único dia, mas em pequenas ações realizadas com consistência todos os dias. 🚀'
+
+print('---------- Exibição da frase original -----------')
+print(f'{frase_motivacional}\n')
+
+print('---------- Exibição da frase em letras maiúsculas -----------')
+print(f'{frase_motivacional.upper()}\n')
+
+print('---------- Exibição da frase em letras minúsculas -----------')
+print(f'{frase_motivacional.lower()}\n')
+
+print('----------- Quantidade de caracteres da frase ----------')
+print(len(frase_motivacional))
---
+++ estrutura_de_dados/exercicios/05_central_mensagens/README.md
@@ -0,0 +1,14 @@
+## Objetivo
+
+Praticar métodos de strings.
+
+## Enunciado
+
+Crie uma frase motivacional.
+
+Exiba:
+
+- A frase original
+- A frase em letras maiúsculas
+- A frase em letras minúsculas
+- A quantidade de caracteres da frase
